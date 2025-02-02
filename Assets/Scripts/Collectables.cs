877067b1e7781cc62ceaf498634f826ed33b9e60
shuerry <105619406+shuerry@users.noreply.github.com>
2025-02-02 17:16:34 -0500

    Basic Pickup Behavior

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -1,17 +1,59 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
+    public static List<GameObject> pickupItems = new List<GameObject>();
+    public float pickupRange = 3f; // Set how close the player needs to be
+    private Transform player;
+    public GameObject pickupPrompt;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
+        if (pickupPrompt != null)
+        {
+            pickupPrompt.SetActive(false); // Hide prompt initially
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        //if (CarnivalLevelManager.isGameOver)
+        //{
+        //    pickupItems.Clear();
+        //    collected = 0;
+        //}
+        if (player == null) return;
+        float distance = Vector3.Distance(transform.position, player.position);
 
+        if (distance <= pickupRange)
+        {
+            if (pickupPrompt != null)
+                pickupPrompt.SetActive(true); // Show prompt
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CollectItem();
+            }
+        }
+        else
+        {
+            if (pickupPrompt != null)
+                pickupPrompt.SetActive(false); // Hide prompt
+        }
+
+    }
+
+    private void CollectItem()
+    {
+        pickupItems.Add(this.gameObject);
+        Debug.Log("Item Collected: " + pickupItems);
+        if (pickupPrompt != null)
+            pickupPrompt.SetActive(false);
+        Destroy(gameObject, 1); // Remove the item from the scene
     }
 }
---
+++ Assets/Scripts/ShootingGameLevelManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+    public Text scoreText;
+    public static bool isGameOver = false;
+    public float time;
+    public Text timerText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        SetTimerText();
+        SetScoreText();
+        if (time <= 0)
+        {
+            time = 0;
+            GameOver();
+        }
+    }
+
+    void SetTimerText()
+    {
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+
+    public void SetScoreText()
+    {
+        scoreText.text = "Score: " + TargetBehavior.scored.ToString();
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        scoreText.text = "Time's up!";
+        timerText.text = $"00:00";
+    }
+}
